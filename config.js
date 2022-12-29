6d6880315fc7b00867aaab91107bfe1df0e58fb8
eazy2003 <102927942+eazy2003@users.noreply.github.com>
2022-12-29 19:17:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -14,31 +14,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['917012984396'] //ganti nomer lu
-global.ownername = "ASWIN SPARKY" //ganti nama lu
-global.developer = "ASWIN" //name
+global.vcardowner = ['263715882540'] //ganti nomer lu
+global.ownername = "EAZY" //ganti nama lu
+global.developer = "EAZY" //name
 global.ytname = "YT: SPARKY MON 777" //ur yt chanel name
 global.socialm = "https://www.instagram.com/aswin_sparky.exe" //taroh sosialmedia lu kalo ga punya biarin aja
 global.sgc = "https://chat.whatsapp.com/JTUrdnOziKD44ScoBoggmh" //ganti group lu
 global.location = "Kerala,TVM,Varkala" //ganti lokasi lu
 
 //bot bomdy 
-global.owner = ['917012984396'] //ur number
-global.ownernomer = "917012984396" //ur number
-global.ownertag = '917012984396' //ur tag number
-global.premium = ['917012984396'] //ur premium number
-global.botname = 'SPARKY MD+' //ur bot name
-global.ownername = "ASWIN SPARKY"
+global.owner = ['263715882540'] //ur number
+global.ownernomer = "263715882540" //ur number
+global.ownertag = '263715882540' //ur tag number
+global.premium = ['263715882540'] //ur premium number
+global.botname = 'EAZY MD+' //ur bot name
+global.ownername = "EAZY"
 global.linkz = "https://youtube.com/@sparkymon777" //your theme url which will be displayed on whatsapp
-global.dana = "917012984396"
-global.pulsa = "917012984396"
+global.dana = "263715882540"
+global.pulsa = "263715882540"
 global.websitex = "https://youtube.com/@sparkymon777" //ur website to be displayed
 global.websitex1 = "https://sparkymodswa.blogspot.com"
-global.botscript = 'https://github.com/Sparkymon777/SPARKY-BOT-MD' //script link
+global.botscript = 'https://github.com/eazy2003/SPARKY-BOT-MD' //script link
 global.themeemoji = "👀" //ur theme emoji
-global.packname = "S P A R K Y" //ur sticker watermark packname
-global.author = "ASWIN" //ur sticker watermark author
-global.wm = "S P A R K Y" //ur watermark
+global.packname = "E A Z Y" //ur sticker watermark packname
+global.author = "EAZY" //ur sticker watermark author
+global.wm = "E A Z Y" //ur watermark
 
 // Other
 global.sessionName = 'session'
